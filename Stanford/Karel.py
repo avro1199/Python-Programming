367bbc652fbe9e785d17b1cad2f7a8b0dd3fc357
Rj Avro <avro1199@gmail.com>
2023-04-05 07:50:56 +0600

    Changes to be committed: 	new file:   Stanford/Karel.py

---
+++ Stanford/Karel.py
@@ -0,0 +1,23 @@
+from karel.stanfordkarel import *
+
+# File: piles.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel
+# pick up all the beepers in the world.
+def main():
+    move()
+    # your code here
+    for i in range(3):
+        for j in range(10):
+            pick_beeper()
+        move()
+        if front_is_clear():
+            move()
+   
+   
+   
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
